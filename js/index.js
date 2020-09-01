4d3f1f185ee1a7300ce8093ca2becd2753cea2c9
Ashley Slater <orecodev@gmail.com>
2020-08-31 20:31:29 -0700

    Done everything except bottom

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br> Is <br> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +40,66 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const butIsIt = document.querySelector("h1")
+butIsIt.innerHTML = siteContent["cta"]["h1"]
+
+const myButton = document.querySelector("button")
+myButton.textContent = siteContent["cta"]["button"]
+
+//Nav Build
+let ourNav = document.getElementsByTagName('a')
+ourNav[0].textContent = siteContent["nav"]["nav-item-1"]
+ourNav[1].textContent = siteContent["nav"]["nav-item-2"]
+ourNav[2].textContent = siteContent["nav"]["nav-item-3"]
+ourNav[3].textContent = siteContent["nav"]["nav-item-4"]
+ourNav[4].textContent = siteContent["nav"]["nav-item-5"]
+ourNav[5].textContent = siteContent["nav"]["nav-item-6"]
+
+//End of Nav Build
+
+const thisImage = document.getElementById("cta-img")
+thisImage.setAttribute('src', siteContent["cta"]["img-src"])
+//Image
+
+
+
+
+
+//Features and About Section
+const goesInTheMiddle = document.getElementsByTagName("h4")
+goesInTheMiddle[0].textContent = siteContent['main-content']['features-h4']
+goesInTheMiddle[1].textContent = siteContent['main-content']['about-h4']
+goesInTheMiddle[2].textContent = siteContent['main-content']['services-h4']
+goesInTheMiddle[3].textContent = siteContent['main-content']['product-h4']
+goesInTheMiddle[4].textContent = siteContent['main-content']['vision-h4']
+
+
+//Middle Image
+const middleImage = document.getElementById("middle-img")
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+//Bottom Section
+const middleWords = document.getElementsByTagName('p')
+middleWords[0].textContent = siteContent['main-content']['features-content']
+middleWords[1].textContent = siteContent['main-content']['about-content']
+middleWords[2].textContent = siteContent['main-content']['services-content']
+middleWords[3].textContent = siteContent['main-content']['product-content']
+middleWords[4].textContent = siteContent['main-content']['vision-content']
+
+
+//Contact 
+
+ 
+
+
+//End of Contact
+
+
+
+//Footer Info
+
+
+
+//Done!
